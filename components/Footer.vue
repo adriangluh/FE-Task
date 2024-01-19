291b6d5a52440db9533d02a623135c4cb24f5948
adriangluh <142819610+adriangluh@users.noreply.github.com>
2024-01-19 09:43:03 +0000

    Added Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+<footer class="footer bg-darkBlue text-white p-10">
+    <div class="footer-top grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
+    <div>
+        <h4 class="footer-heading">CarDealer uk</h4>
+        <address class="not-italic">
+        101 Business Street<br>
+        Business Town<br>
+        Business County<br>
+        BU51 N55
+        </address>
+        <p>Telephone: 01234 567890</p>
+        <p>Email: enquiries@cardealer.uk</p>
+    </div>
+    </div>
+    
+    <div class="footer-bottom grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div>
+        <p>Company Info</p>
+        <p>Registered in England and Wales.</p>
+        <p>Registered No: 00000000 | VAT No: 000 0000 00</p>
+        <p>Registered Office: 101 Business Street, Business Town, Business County BU51 N55.</p>
+    </div>
+    <div>
+        <p>Financial Disclosure</p>
+    </div>
+    </div>
+    
+    <div class="footer-credit mt-8 text-center text-sm">
+        <a href="https://www.nexuspoint.co.uk/" alt="Nexus Point">Powered by Nexus Point</a> 
+        <p>© 2024 Adrian Gluh | FE - Task</p>
+    </div>
+</footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.footer-top,
+.footer-bottom {
+    @apply grid gap-8;
+}
+
+</style>
+
+
+
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,25 @@
 
 <template>
     <div>
-        <Navbar />
-        <Nuxt />
+        <main>
+            <Navbar />
+            <Nuxt />
+        </main>
         <Footer />
     </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+}
+
+</script>
 
 <style></style>
