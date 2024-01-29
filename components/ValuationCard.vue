3f1f1e9f311b022ee3e96d11b7e2017d28c6e535
adriangluh <142819610+adriangluh@users.noreply.github.com>
2024-01-29 21:05:31 +0000

    Update the valuation card to match the design

--- components/ValuationCard.vue
+++ components/ValuationCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="car-item valuation-card bg-white shadow rounded-lg overflow-hidden p-6 flex flex-col items-center justify-between h-full">
-        <h2 class="valuation-title text-center text-2xl font-bold mb-2">Value my car</h2>
-        <p class="text-center">Find out the value of your car in just a few minutes.</p>
+    <div class="car-item valuation-card shadow rounded-lg overflow-hidden p-11 flex flex-col items-center justify-between h-full">
+        <h2 class="valuation-title text-center text-l">Value your car</h2>
+        <p class="find-value-text text-center">Find out the value of your car in just a few minutes.</p>
         <div class="input-wrapper px-5">
             <form @submit.prevent="submitValuation">
             <input
@@ -9,17 +9,17 @@
                 v-model="vrm"
                 placeholder="Enter VRM"
                 required
-                class="input-field mb-2"
+                class="input-field mb-6"
             />
             <input
                 type="number"
                 v-model="mileage"
                 placeholder="Enter mileage"
                 required
-                class="input-field mb-2"
+                class="input-field mb-7"
             />
-            <button type="submit" class="value-my-car-btn w-full ">Value my car</button>
-            </form>
+        </form>
+        <button type="submit" class="value-my-car-btn w-full rounded-lg">Value my car</button>
     </div>
     </div>
 </template>
@@ -42,9 +42,9 @@
 
 <style scoped lang="scss">
 .input-field {
-    @apply w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500;
+    @apply w-full px-4 py-3 border text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500;
 }
 .value-my-car-btn {
-    @apply text-white font-bold py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50;
+    @apply text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50;
 }
 </style>
